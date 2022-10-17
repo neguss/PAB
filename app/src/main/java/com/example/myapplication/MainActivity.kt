d1167a3f96ebc7276289aee792cbe0c417cce2ba
neg <personalnegus@gmail.com>
2022-10-17 15:35:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val start_button=findViewById<Button>(R.id.start_btn)
+        val chk_activity=Intent(this,CheckActivity::class.java)
+        start_button.setOnClickListener{
+            startActivity(chk_activity)
+        }
+    }
+}
