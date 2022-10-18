9ef3c2f5a83aee42bd5d9a0a64af1b1c6afa1c1a
neg <personalnegus@gmail.com>
2022-10-18 14:56:58 +0300

    created view for main list

--- app/src/main/java/com/example/myapplication/CheckActivity.kt
+++ app/src/main/java/com/example/myapplication/CheckActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 
 class CheckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
