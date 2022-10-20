0eb8779c28017c99c31bf5d186d9ad38f034774f
neg <personalnegus@gmail.com>
2022-10-20 15:22:52 +0300

    implemented callback from CheckActivity.kt and function for saving files on external storage

--- app/src/main/java/com/example/myapplication/CheckActivity.kt
+++ app/src/main/java/com/example/myapplication/CheckActivity.kt
@@ -2,13 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.CheckBox
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.children
@@ -24,7 +22,6 @@ class CheckActivity : Activity() {
         var tv= emptyArray<View>()
         var chk1= emptyArray<View>()
         var chk2= emptyArray<View>()
-
         for (v in data_list.children){//separate fly's from rows
             when(v.id){
                 R.id.p1,R.id.p2,R.id.p3,R.id.p4,R.id.p5,R.id.p6,R.id.p7 -> headers+=v
@@ -37,21 +34,33 @@ class CheckActivity : Activity() {
             chk1+=tmp.getChildAt(1)
             chk2+=tmp.getChildAt(2)
         }
-        var chk_results=Array<Int>(chk1.size) { 0 }
+        var chk_results=ByteArray(chk1.size){0}
 
         for (i in chk1.indices){
             chk1[i].setOnClickListener{
                 var tmp1=findViewById<CheckBox>(chk1[i].id)
                 var tmp2=findViewById<CheckBox>(chk2[i].id)
-                if (tmp1.isChecked)
-                    tmp2.isChecked=false
+                if (tmp1.isChecked) {
+                    chk_results[i]=1
+                    tmp2.isChecked = false
+                }
             }
             chk2[i].setOnClickListener{
                 var tmp1=findViewById<CheckBox>(chk1[i].id)
                 var tmp2=findViewById<CheckBox>(chk2[i].id)
-                if (tmp2.isChecked)
-                    tmp1.isChecked=false
+                if (tmp2.isChecked) {
+                    chk_results[i]=2
+                    tmp1.isChecked = false
+                }
             }
+        }
+
+        var save_btn=findViewById<Button>(R.id.save_btn)
+        save_btn.setOnClickListener{
+            var intent=Intent()
+            intent.putExtra("results",chk_results)
+            setResult(2,intent)
+            finish()
         }
         Toast.makeText(this,"213",Toast.LENGTH_SHORT)
     }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,15 +5,82 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import java.io.File
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : Activity() {
+
+    lateinit var chk_list:ByteArray
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val npp=findViewById<EditText>(R.id.npp_value)
+        val subdivision=findViewById<EditText>(R.id.subdivision_value)
+        val site=findViewById<EditText>(R.id.site_value)
+        val job=findViewById<EditText>(R.id.job_value)
+        val dt=findViewById<EditText>(R.id.date_value)
+        val watchers=findViewById<EditText>(R.id.watchers_value)
+        val tm=findViewById<EditText>(R.id.time_value)
         val start_button=findViewById<Button>(R.id.start_btn)
+        val save_button=findViewById<Button>(R.id.Save)
         val chk_activity=Intent(this,CheckActivity::class.java)
+
+        var curr_dt=LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).toString()
+
         start_button.setOnClickListener{
-            startActivity(chk_activity)
+            startActivityForResult(chk_activity,1)
+        }
+
+        save_button.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(applicationContext,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED) {
+                val out = File("/storage/emulated/0/Download/"+"Протокол "+npp.text+"_от_"+curr_dt.replace('/','.')+".csv")
+                if (!out.exists())
+                    out.createNewFile()
+                else {
+                    out.delete()
+                    out.createNewFile()
+                }
+                out.appendText("Номер\t\t"+npp.text+"\n")
+                out.appendText("Подразделение\t\t"+subdivision.text+"\n")
+                out.appendText("Участок\t\t"+site.text+"\n")
+                out.appendText("Выполняемая работа\t\t"+job.text+"\n")
+                out.appendText("Дата\t\t"+dt.text+"\n")
+                out.appendText("Кол-во наблюдателей\t\t"+watchers.text+"\n")
+                out.appendText("Время\t\t"+tm.text+"\n")
+
+                for (i in chk_list.indices) {
+                    out.appendText(chk_list[i].toString()+",")
+                }
+            }
+            else {
+                Toast.makeText(this, "Нет доступа к внутреннему хранилищу", Toast.LENGTH_SHORT)
+                ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),12 )
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data==null)
+            return
+        if (resultCode==2 && requestCode==1){
+            chk_list= data.getByteArrayExtra("results")!!
+            Toast.makeText(this,"done",Toast.LENGTH_SHORT)
         }
     }
 }
+/*
+
+ */
