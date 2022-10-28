a6f4b11cb6898aaa6457d9e86fa9cc7f995d2e39
neg <personalnegus@gmail.com>
2022-10-28 09:26:21 +0300

    added new field for auditor name and credentials

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import androidx.core.content.ContextCompat
 import java.io.File
 import android.content.pm.PackageManager
+import android.icu.text.SymbolTable
 import android.os.Build
 import android.widget.*
 import androidx.annotation.RequiresApi
@@ -14,6 +15,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.requestPermissions
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import kotlin.math.cos
 
 class MainActivity : Activity() {
 
@@ -34,12 +36,33 @@ class MainActivity : Activity() {
         val start_button=findViewById<Button>(R.id.start_btn)
         val save_button=findViewById<Button>(R.id.Save)
         val chk_activity=Intent(this,CheckActivity::class.java)
+        var test1:String="Это тестовая строка, смирись"
+        var test2:String=""
+        var test3:String=""
+        var output:String=""
+        var nm:String="1_от_27.10.2022"
         var pers= arrayOf("Басов С.А.","Божко Д.В","Бойко Д.В","Большаков В.В.","Ворыпаев С.В","Егупов Н.А.","Зелепукин Д.А.","Илюшов И.Н.","Кайряк А.М.","Королев А.М.","Кузнецов А.С.","Куницин С.Н.","Левин А.А.","Логач Е.С.","Лямин П.И.","Миндагалиев Р.С.","Морозов Р.А.","Морхов Д.В.","Никитенко А.А.","Никитин А.А.","Потапов А.В.","Рудов М.В.","Скворцов И.А.","Смирнов Р.В.","Сухов В.А.","Халеев С.И.","Черпита О.В.","Чурилов Э.В.","Шацких С.А.")
         var auditorlvl= arrayOf(1,2,2,3,3,3,3,3,3,3,2,3,2,1,3,3,3,3,2,2,3,3,2,3,3,3,3,1,3)
         var auditor= arrayOf("Начальник филиала","Ведущий инженер-энергетик","Старший мастер по ремонту скважин (капитальному, подземному)","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Механик","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Начальник цеха","Мастер по ремонту (капитальному,подземному)","Начальник базы","Заместитель главного инженера по ОТ, ПБ,П и ООС","Инженер по КИПиА","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Старший мастер по ремонту скважин (капитальному, подземному)","Старший мастер по ремонту скважин (капитальному, подземному)","Старший механик","Старший механик","Ведущий инженер по ремонту","Мастер по ремонту","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Мастер по ремонту (капитальному,подземному)","Главный инженер","Мастер по ремонту (капитальному,подземному)")
         var persadapt=ArrayAdapter(this,com.google.android.material.R.layout.mtrl_auto_complete_simple_item,pers)
         fio.setAdapter(persadapt)
         var curr_dt=LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).toString()
+        val shift:Int= (cos((test1[0].code+test1[1].code).toDouble()) *352).toInt()
+        for (i in 0..test1.length-1){
+            if (i%2==0){
+                test2+=Char((test1[i].code-shift))
+            }else{
+                test2+=Char((test1[i].code-shift*2))
+            }
+        }
+
+        for (i in 0..test2.length-1){
+            if (i%2==0){
+                test3+=Char((test2[i].code+shift))
+            }else{
+                test3+=Char((test2[i].code+shift*2))
+            }
+        }
 
         dt.text=curr_dt
         start_button.setOnClickListener{
@@ -55,25 +78,33 @@ class MainActivity : Activity() {
                     out.delete()
                     out.createNewFile()
                 }
-                out.appendText(""+npp.text +",")
-                out.appendText(""+fio.text+",")
+
+                //out.appendText(""+npp.text +",")
+                //out.appendText(""+fio.text+",")
                 var chosenone=pers.indexOf(fio.text.toString())
-                out.appendText(""+auditor[chosenone]+",")
-                out.appendText(""+auditorlvl[chosenone]+",")
-                out.appendText(""+subdivision.text+",")
-                out.appendText(""+site.text+",")
-                out.appendText(""+job.text+",")
-                out.appendText(""+dt.text+",")
-                out.appendText(""+watchers.text+",")
-                out.appendText(""+tm.text+",")
+                //out.appendText(""+auditor[chosenone]+",")
+                //out.appendText(""+auditorlvl[chosenone]+",")
+                //out.appendText(""+subdivision.text+",")
+                //out.appendText(""+site.text+",")
+                //out.appendText(""+job.text+",")
+                //out.appendText(""+dt.text+",")
+                //out.appendText(""+watchers.text+",")
+                //out.appendText(""+tm.text+",")
+                output=""+npp.text +","+fio.text+","+auditor[chosenone]+","+auditorlvl[chosenone]+","+subdivision.text+","+site.text+","+job.text+","+dt.text+","+watchers.text+","+tm.text+","
 
                 for (i in chk_list.indices) {
-                    if(i!=chk_list.size-1)
-                        out.appendText(chk_list[i].toString()+",")
-                    else
-                        out.appendText(chk_list[i].toString())
+                    if(i!=chk_list.size-1){
+                        //out.appendText(chk_list[i].toString()+",")
+                        output+=chk_list[i].toString()+","}
+                    else{
+                        output+=(chk_list[i].toString())
+                        //output += chk_list[i].toString()
+                    }
                 }
-                Toast.makeText(this, "Отчет успешно сохранен по пути"+"/storage/emulated/0/Download/"+"Протокол "+npp.text+"_от_"+curr_dt.replace('/','.')+".csv", Toast.LENGTH_SHORT)
+                var testt=output.toByteArray()
+                //
+                out.appendText(output)
+                Toast.makeText(this, "Отчет успешно сохранен по пути"+"/storage/emulated/0/Download/"+npp.text+"_от_"+curr_dt.replace('/','.'), Toast.LENGTH_SHORT)
             }
             else {
                 Toast.makeText(this, "Нет доступа к внутреннему хранилищу", Toast.LENGTH_SHORT)
